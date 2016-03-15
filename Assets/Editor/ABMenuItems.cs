efe6f5a9bc56b9fe3e86a6b8756c749dbd39b965
Dori Chan <dori.p.chan@gmail.com>
2016-03-15 11:21:30 -0700

    initial commit
    - added images to use as a test asset bundle
    - started scripts to handle creating the asset bundle for upload

---
+++ Assets/Editor/ABMenuItems.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace AssetBundles 
+{
+	public class ABMenuItems : MonoBehaviour 
+	{
+		[MenuItem("Assets/AssetBundles/Build Asset Bundles")]
+		static public void BuildAssetBundles()
+		{
+			AssetBundleManager.BuildAssetBundles();
+		}
+	}
+}
